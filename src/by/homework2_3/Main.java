422e033a6b82021343b029266d0ea429b8ae6633
AndreyShutikov <andrey.schutikov@yandex.ru>
2020-10-06 06:49:32 +0300

    Initial commit

---
+++ src/by/homework2_3/Main.java
@@ -0,0 +1,96 @@
+package by.homework2_3;
+
+import java.util.*;
+
+public class Main {
+    static final String[] UNIQUE_WORDS = {"account", "act", "addition", "adjust","advertisement","back","birth", "make ","still","any","take",  "little","south" };
+
+
+    static Scanner read = new Scanner(System.in);
+    static PhoneBook phoneBook = new PhoneBook();
+
+    public static void main(String[] args) {
+
+        String[] words = generateArrayOfWords(30, UNIQUE_WORDS);
+        Set<String> setWords = new HashSet<>(Arrays.asList(words));
+
+        List<String> listWords = new ArrayList<>(Arrays.asList(setWords.toArray(new String[setWords.size()])));
+        System.out.println("Start array of the words:\n" + new ArrayList<>(Arrays.asList(words)));
+        System.out.println("List of the unique words:\n" + listWords);
+        quantityOfRepetition(generateMapOfWords(listWords, words));
+
+        PhoneBook phoneBook = new PhoneBook();
+        System.out.println();
+        printPhones(phoneBook);
+        System.out.println();
+        printEmail(phoneBook);
+
+
+        System.out.println();
+        userChangeFirstName();
+    }
+
+    static void userChangeFirstName() {
+        String firstNameEnteredByUser;
+        while (true) {
+            System.out.println("Enter '0' for exit\nEnter Firstname: from " + getPersonFirstNames(phoneBook));
+            firstNameEnteredByUser = read.nextLine();
+            if (firstNameEnteredByUser.equals("0")) {
+                break;
+            } else {
+                System.out.println(phoneBook.getPhonesList(new Person(firstNameEnteredByUser)));
+                System.out.println(phoneBook.getEmailList(new Person(firstNameEnteredByUser)));
+            }
+        }
+        }
+
+    static String[] generateArrayOfWords(int size, String[] basicWords) {
+        Random random = new Random();
+        String[] words = new String[size];
+        for (int i = 0; i < size; i++) {
+            words[i] = basicWords[random.nextInt(basicWords.length)];
+        }
+        return words;
+    }
+    static HashMap<String, Integer> generateMapOfWords(List<String> listWords, String[] words) {
+        HashMap<String, Integer> mapWords = new HashMap<>();
+        int quantity;
+        for (String listWord : listWords) {
+            quantity = 0;
+            for (String arrayWord : words) {
+                if (listWord.equals(arrayWord)) {
+                    mapWords.put(listWord, ++quantity);
+                }
+            }
+        }
+        return mapWords;
+    }
+    static void quantityOfRepetition(HashMap<String, Integer> mapWords) {
+        for (Map.Entry<String, Integer> entryWord : mapWords.entrySet()) {
+            System.out.println("The word '" + entryWord.getKey() + "' repeats into the list " + entryWord.getValue() + " times");
+        }
+    }
+    static void printPhones(PhoneBook phoneBook) {
+        Set<String> persons = getPersonFirstNames(phoneBook);
+        for (String personFirstName : persons) {
+            System.out.println(personFirstName + "'s phones: " + phoneBook.getPhonesList(new Person(personFirstName)));
+        }
+    }
+
+
+        static void printEmail(PhoneBook phoneBook) {
+            Set<String> persons = getPersonFirstNames(phoneBook);
+            for (String personFirstName : persons) {
+                System.out.println(personFirstName + "'s email: " + phoneBook.getEmailList(new Person(personFirstName)));
+            }
+        }
+
+
+    static Set<String> getPersonFirstNames(PhoneBook phoneBook) {
+        Set<String> persons = new HashSet<>();
+        for (int i = 0; i < phoneBook.getGeneratedPersons().length; i++) {
+            persons.add(phoneBook.generatePersons()[i].getFirstName());
+        }
+        return persons;
+    }
+}
